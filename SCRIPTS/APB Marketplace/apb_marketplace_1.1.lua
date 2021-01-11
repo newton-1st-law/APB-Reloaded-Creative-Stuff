c22a77ec7d6106ac7ac96b5d185e3cef2d57046e
PriMan92 <priman92@gmail.com>
2021-01-12 01:08:26 +0300

    January update
    - сompleted main menu PSD file
    - some changes in scripts
    - updated banner PSD file

--- SCRIPTS/APB Marketplace/apb_marketplace_1.1.lua
+++ SCRIPTS/APB Marketplace/apb_marketplace_1.1.lua
@@ -79,6 +79,17 @@ function StepMouseWheel(mmw)
 	end
 end
 
+function SortList(sl)
+	
+	sl = true;
+	
+	if sl == true then
+		
+		ClickPosition(48494, 13241);
+		sl = false;
+	end
+end
+
 function SirenMode1(mode)
 
 	local step = 0;
@@ -115,30 +126,11 @@ function SirenMode3(mode)
 	end
 end
 
---[[ function PlayAnimation(anim)
-
-	local layout = getlayout();
-	OutputLogMessage(layout);
-	
-	if getlayout() == "409" then
-		var = setlayout(419)
-		Sleep(50);
-		
-		if anim == hello then
-			PressAndReleaseKey("g", "h", "b", "d", "t", "n");
-		end
-	else
-		if anim == hello then
-			PressAndReleaseKey("g", "h", "b", "d", "t", "n");
-		end
-	end
- end ]]
-
 function OnEvent(event, arg)
 
 	current_mkey = GetMKeyState();
 	
-    if (event == "G_PRESSED" and arg == 6) and current_mkey == 1 then -- REMOVING MAILS
+    if (event == "G_PRESSED" and arg == 6) and current_mkey == 2 then -- REMOVING MAILS
         
 		ClickPosition(27594, 20347);
         ClickPosition(47503, 33344);
@@ -146,25 +138,27 @@ function OnEvent(event, arg)
         ClickPosition(39034, 51566);
         ClickPosition(32785, 35106);
 
-    elseif (event == "G_PRESSED" and arg == 7) and current_mkey == 1 then -- SALE OF SYMBOLS
+    elseif (event == "G_PRESSED" and arg == 7) and current_mkey == 2 then -- SALE OF SYMBOLS
 	
-        ClickPosition(13489, 11783);
+		ClickPosition(13489, 11783);
+--		SortList();
         ClickPosition(10211, 58125);
         ClickPosition(23803, 20893);
         ClickPosition(20968, 29700);
 		StepMouseWheel(75);
 		ClickPosition(28721, 46524);
 
-    elseif (event == "G_PRESSED" and arg == 8) and current_mkey == 1 then -- SALE OF CLOTHES
+    elseif (event == "G_PRESSED" and arg == 8) and current_mkey == 2 then -- SALE OF CLOTHES
 	
         ClickPosition(13489, 11783);
+--		SortList();
         ClickPosition(10211, 58125);
         ClickPosition(23803, 20893);
         ClickPosition(20934, 26785);
 		StepMouseWheel(75);
 		ClickPosition(28721, 46524);
 		
-    elseif (event == "G_PRESSED" and arg == 1) and current_mkey == 1 then -- PRICE: 2147483646 - 2147483646
+    elseif (event == "G_PRESSED" and arg == 1) and current_mkey == 2 then -- PRICE: 2147483646 - 2147483646
 	
         ClickPosition(43952, 49015);
         ClickPosition(31794, 24902);
@@ -173,7 +167,7 @@ function OnEvent(event, arg)
 		PressAndReleaseKey("2", "1", "4", "7", "4", "8", "3", "6", "4", "6");
         ClickPosition(37019, 42576);
 		
-    elseif (event == "G_PRESSED" and arg == 2) and current_mkey == 1 then -- PRICE: 25000 - 31250
+    elseif (event == "G_PRESSED" and arg == 2) and current_mkey == 2 then -- PRICE: 25000 - 31250
 	
         ClickPosition(43952, 49015);
         ClickPosition(31794, 24902);
@@ -182,7 +176,7 @@ function OnEvent(event, arg)
 		PressAndReleaseKey("3", "1", "2", "5", "0");
         ClickPosition(37019, 42576);
 		
-    elseif (event == "G_PRESSED" and arg == 3) and current_mkey == 1 then -- PRICE: 18750 - 25000
+    elseif (event == "G_PRESSED" and arg == 3) and current_mkey == 2 then -- PRICE: 18750 - 25000
 	
         ClickPosition(43952, 49015);
         ClickPosition(31794, 24902);
@@ -191,7 +185,7 @@ function OnEvent(event, arg)
 		PressAndReleaseKey("2", "5", "0", "0", "0");
         ClickPosition(37019, 42576);
 		
-    elseif (event == "G_PRESSED" and arg == 4) and current_mkey == 1 then -- PRICE: 12500 - 18750
+    elseif (event == "G_PRESSED" and arg == 4) and current_mkey == 2 then -- PRICE: 12500 - 18750
 	
         ClickPosition(43952, 49015);
         ClickPosition(31794, 24902);
@@ -200,7 +194,7 @@ function OnEvent(event, arg)
 		PressAndReleaseKey("1", "8", "7", "5", "0");
         ClickPosition(37019, 42576);
 		
-    elseif (event == "G_PRESSED" and arg == 5) and current_mkey == 1 then -- PRICE: 6250 - 12500
+    elseif (event == "G_PRESSED" and arg == 5) and current_mkey == 2 then -- PRICE: 6250 - 12500
 	
         ClickPosition(43952, 49015);
         ClickPosition(31794, 24902);
@@ -209,7 +203,7 @@ function OnEvent(event, arg)
 		PressAndReleaseKey("1", "2", "5", "0", "0");
         ClickPosition(37019, 42576);
 
-    elseif (event == "G_PRESSED" and arg == 9) and current_mkey == 1 then -- GET MOUSE POSITION \ MAKE A ONE COPY OF ITEM
+    elseif (event == "G_PRESSED" and arg == 9) and current_mkey == 2 then -- GET MOUSE POSITION \ MAKE A ONE COPY OF ITEM
         
 		x, y = GetMousePosition();
 		OutputLogMessage("Mouse is at %d, %d\n", x, y);
@@ -217,25 +211,17 @@ function OnEvent(event, arg)
         ClickPosition(42244, 41665);
         ClickPosition(36336, 38568);
 		
-    elseif (event == "G_PRESSED" and arg == 1) and current_mkey == 2 then -- USE SIREN MODE 1
+    elseif (event == "G_PRESSED" and arg == 1) and current_mkey == 3 then -- USE SIREN MODE 1
         
 		SirenMode1(2);
 		
-    elseif (event == "G_PRESSED" and arg == 2) and current_mkey == 2 then -- USE SIREN MODE 2
+    elseif (event == "G_PRESSED" and arg == 2) and current_mkey == 3 then -- USE SIREN MODE 2
         
 		SirenMode2(2);
 		
-    elseif (event == "G_PRESSED" and arg == 3) and current_mkey == 2 then -- USE SIREN MODE 3
+    elseif (event == "G_PRESSED" and arg == 3) and current_mkey == 3 then -- USE SIREN MODE 3
         
 		SirenMode3(2);
-		
---[[    elseif (event == "G_PRESSED" and arg == 1) and current_mkey == 3 then -- USE ANIMATION
-        
-		PressAndReleaseKey("backslash"); -- BACKSLASH
-		Sleep(50);
-		PlayAnimation(hello);
-		Sleep(50);
-		PressAndReleaseKey("enter"); ]]
 		
 	end
 end
